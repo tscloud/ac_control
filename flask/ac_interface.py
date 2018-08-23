6df3d599f8f82b24376771769bf633b000f2f197
tscloud <thomas.s.cloud@gmail.com>
2018-08-23 16:26:56 -0400

    simple web interface for pin toggle

--- flask/ac_interface.py
+++ flask/ac_interface.py
@@ -6,24 +6,16 @@ from flask import Flask, render_template
 from collections import OrderedDict
 app = Flask(__name__)
 
-#read config file
-config = ConfigParser.RawConfigParser()
-### I think this is weird -- have to do this to make the options not convert to lowercase
-config.optionxform = str
-# hardcoded - not sure how to pass/set config file
-config.read('..\\.config_pigosemipro.cfg')
-
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = OrderedDict()
-pins[config.getint('Pins', 'CAMERAOUT')] = {'name' : 'camera button', 'state' : 1}
-pins[config.getint('Pins', 'PLAYEROUT')] = {'name' : 'player button', 'state' : 1}
-pins[config.getint('Pins', 'PROGRUNNINGLEDOUT')] = {'name' : 'running light', 'state' : 0}
-print 'pins dict: %s' % pins
+# OUT pin used to contol whatever
+CONTROLPIN = 15
+# msg describing current state of pin
+pinStateMsg = "on"
 
 @app.route("/")
 def main():
     templateData = {
-        'pins' : pins
+        'startStopBtnMsg' : pinStateMsg
+        'pin' : CONTROLPIN
         }
     # Pass the template data into the template main_GPIO.html and return it to the user
     return render_template('main_GPIO.html', **templateData)
@@ -34,7 +26,7 @@ def action(changePin):
     # Convert the pin from the URL into an integer:
     changePin = int(changePin)
     # Get the device name for the pin being changed:
-    deviceName = pins[changePin]['name']
+    deviceName = "A/C unit"
     # simulate button press
     # Read the pin and set it to whatever it isn't (that is, toggle it) and then set it back:
     time.sleep(0.2)
@@ -42,8 +34,9 @@ def action(changePin):
 
     # Along with the pin dictionary, put the message into the template data dictionary:
     templateData = {
+        'startStopBtnMsg' : pinStateMsg
+        'pin' : CONTROLPIN
         'message' : message,
-        'pins' : pins
     }
 
     return render_template('main_GPIO.html', **templateData)
